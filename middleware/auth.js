fe3ac3436c3cffb3d3c59c123d9bf99d58feb9f6
edemfrancis <efrancis.edem@gmail.com>
2026-03-30 15:39:18 +0100

    Updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 exports.authenticate = (req, res, next) => {
 	if (req.session.user === undefined) {
 		return res
-			.status(401)
+			.status(400)
 			.json({ message: "Access denied. No session provided." });
 	}
 	next();
